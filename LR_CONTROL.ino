bd2a9b6730a7beca7b84feeeece4fd7e1452a37a
John Phillips <john.h.phillips@clegadgetworks.com>
2013-09-03 10:35:20 -0700

    More detailed setup, monitor control input

--- LR_CONTROL.ino
+++ LR_CONTROL.ino
@@ -1,85 +1,72 @@
-// Define digital input pins that monitor control box
-#define WINCH_UP        2
-#define WINCH_UP_SLOW   3
-#define WINCH_DOWN      4
+// define input pins that monitor control box
+#define WINCH_UP 2
+#define WINCH_UP_SLOW 3
+#define WINCH_DOWN 4
 #define WINCH_DOWN_SLOW 5
 
-// Define output pins that drive motor controllers
-#define WINCH_AHI     6
-#define WINCH_BHI     7
-#define WINCH_PWM     9
+// define output pins that drive motor controllers
+#define WINCH_PWM 9 
 
-#define MC_DISABLE    A1
-
-// Define static PWM values
+// define PWM values
 #define zero_pwr 0
 #define half_pwr 127
 #define full_pwr 225
 
-// Define other variables
-int control_input = 0;
-int prev_control_input = 0;
+volatile int control_input = 0;
+volatile int prev_control_input = 0;
 
-// Initial microcontroller setup
-void setup() {
+void setup()  { 
   Serial.begin(9600);
+  // Setup control box input, active low
   pinMode(WINCH_UP, INPUT);
   pinMode(WINCH_UP_SLOW, INPUT);
   pinMode(WINCH_DOWN, INPUT);
   pinMode(WINCH_DOWN_SLOW, INPUT);
+  // Turn on pullup resistors for control box input
+  digitalWrite(WINCH_UP, HIGH);
+  digitalWrite(WINCH_UP_SLOW, HIGH);
+  digitalWrite(WINCH_DOWN, HIGH);
+  digitalWrite(WINCH_DOWN_SLOW, HIGH);
   
-  pinMode(WINCH_AHI, OUTPUT);
-  pinMode(WINCH_BHI, OUTPUT);
   pinMode(WINCH_PWM, OUTPUT);
-  pinMode(MC_DISABLE, OUTPUT);
-}
-void loop()  { 
-  // check control box switches binary to decimal
-  // 0001 = WINCH_UP = 1
-  // 0010 = WINCH_UP_SLOW = 2
-  // 0100 = WINCH_DOWN = 4
-  // 1000 = WINCH_DOWN_SLOW = 8
-  control_input = digitalRead(WINCH_UP) + (digitalRead(WINCH_UP_SLOW) << 1) + 
-  (digitalRead(WINCH_DOWN) << 2) + (digitalRead(WINCH_DOWN_SLOW) << 3);
-  // check current control input against previous control input
-  if( control_input != prev_control_input) {
-    switch( control_input) {
-     case 1:                          // WINCH_UP
-      digitalWrite(WINCH_AHI, HIGH);
-      analogWrite(WINCH_PWM, full_pwr);
-      Serial.print("UP!\n");
-      break;
-    
-     case 2:                          // WINCH_UP_SLOW
-      digitalWrite(WINCH_AHI, HIGH);
-      analogWrite(WINCH_PWM, half_pwr);
-      Serial.print("UP SLOW!\n");
-      break;
-     
-     case 4:                          // WINCH_DOWN
-      digitalWrite(WINCH_BHI, HIGH);
-      analogWrite(WINCH_PWM, full_pwr);
-      Serial.print("DOWN!\n");
-      break;
-      
-     case 8:                          // WINCH_DOWN_SLOW
-      digitalWrite(WINCH_BHI, HIGH);
-      analogWrite(WINCH_PWM, half_pwr);
-      Serial.print("DOWN SLOW!\n");
-      break;
-     
-     default:
-      digitalWrite(WINCH_AHI, LOW);
-      digitalWrite(WINCH_BHI, LOW);
-      analogWrite(WINCH_PWM, 0);
-      Serial.print("IDLE\n");
-    }
-  }
-  // change previous input then loop again
-  prev_control_input = control_input;
-}
-
-void read_input() {
   
+} 
+
+void loop()  { 
+  // read control input (pin 2)
+  control_input = PIND & B00111100;
+  
+  if( control_input != prev_control_input) {
+//    Serial.print(control_input, BIN);
+//    Serial.print("\n");
+    switch ( control_input){
+      case B00111000:    // Winch up at full speed
+        analogWrite(WINCH_PWM, full_pwr);    
+        Serial.print("WINCH UP\n");
+        break;
+       
+      case B00110100:    // Winch up at half speed
+        analogWrite(WINCH_PWM, half_pwr);
+        Serial.print("WINCH UP SLOW\n");
+        break; 
+      
+      case B00101100:    // Winch down at full speed
+        analogWrite(WINCH_PWM, full_pwr);
+        Serial.print("WINCH DOWN\n");
+        break;
+      
+      case B00011100:    // Winch down at half speed
+        analogWrite(WINCH_PWM, half_pwr);
+        Serial.print("WINCH DOWN SLOW\n");
+        break;
+        
+      default:          // Winch disabled
+        analogWrite(WINCH_PWM, zero_pwr);
+        Serial.print("SYSTEM IDLE\n");
+        break;
+    }
+    // move current to previous before next loop
+    prev_control_input = control_input;
+    }
 }
 
