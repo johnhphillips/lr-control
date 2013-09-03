6786cd9cdec253c26c53a53cb0202bcc9354a9d2
John Phillips <john.h.phillips@clegadgetworks.com>
2013-09-03 10:46:51 -0700

    Updated definitions, added control output to switch statement.

--- LR_CONTROL.ino
+++ LR_CONTROL.ino
@@ -1,67 +1,86 @@
-// define input pins that monitor control box
-#define WINCH_UP 2
-#define WINCH_UP_SLOW 3
-#define WINCH_DOWN 4
-#define WINCH_DOWN_SLOW 5
+// define input pins that monitor control box, three binary digits
+#define CONTROL_A0 4
+#define CONTROL_A1 5
+#define CONTROL_A2 6
 
 // define output pins that drive motor controllers
-#define WINCH_PWM 9 
+#define WINCH_AHI 7
+#define WINCH_BHI 8
+#define WINCH_PWM 9
+
+#define TROLLEY_AHI 12
+#define TROLLEY_BHI 13
+#define TROLLEY_FORE_PWM 10
+#define TROLLEY_AFT_PWM 11
 
 // define PWM values
 #define zero_pwr 0
-#define half_pwr 127
+#define half_pwr 25
 #define full_pwr 225
 
 volatile int control_input = 0;
 volatile int prev_control_input = 0;
 
 void setup()  { 
+  // Setup serial output to port (debugging)
   Serial.begin(9600);
   // Setup control box input, active low
-  pinMode(WINCH_UP, INPUT);
-  pinMode(WINCH_UP_SLOW, INPUT);
-  pinMode(WINCH_DOWN, INPUT);
-  pinMode(WINCH_DOWN_SLOW, INPUT);
+  pinMode(CONTROL_A0, INPUT);
+  pinMode(CONTROL_A1, INPUT);
+  pinMode(CONTROL_A2, INPUT);
+  
   // Turn on pullup resistors for control box input
-  digitalWrite(WINCH_UP, HIGH);
-  digitalWrite(WINCH_UP_SLOW, HIGH);
-  digitalWrite(WINCH_DOWN, HIGH);
-  digitalWrite(WINCH_DOWN_SLOW, HIGH);
+  digitalWrite(CONTROL_A0, HIGH);
+  digitalWrite(CONTROL_A1, HIGH);
+  digitalWrite(CONTROL_A2, HIGH);
   
+  // Setup outputs to winch motor control
+  pinMode(WINCH_AHI, OUTPUT);
+  pinMode(WINCH_BHI, OUTPUT);  
   pinMode(WINCH_PWM, OUTPUT);
-  
+  // Setup outputs to trolley motor controls
+  pinMode(TROLLEY_AHI, OUTPUT);
+  pinMode(TROLLEY_BHI, OUTPUT);
+  pinMode(TROLLEY_FORE_PWM, OUTPUT);
+  pinMode(TROLLEY_AFT_PWM, OUTPUT);
 } 
 
 void loop()  { 
-  // read control input (pin 2)
-  control_input = PIND & B00111100;
+  // read control input 
+  control_input = PIND & B01110000;
   
   if( control_input != prev_control_input) {
-//    Serial.print(control_input, BIN);
-//    Serial.print("\n");
+    Serial.print(control_input, BIN);
+    Serial.print("\n");
     switch ( control_input){
-      case B00111000:    // Winch up at full speed
-        analogWrite(WINCH_PWM, full_pwr);    
+      case B01100000:    // Winch up at full speed
+        analogWrite(WINCH_PWM, full_pwr);
+        digitalWrite(WINCH_AHI, HIGH);    
         Serial.print("WINCH UP\n");
         break;
        
-      case B00110100:    // Winch up at half speed
+      case B01010000:    // Winch up at half speed
         analogWrite(WINCH_PWM, half_pwr);
+        digitalWrite(WINCH_AHI, HIGH);
         Serial.print("WINCH UP SLOW\n");
         break; 
       
-      case B00101100:    // Winch down at full speed
+      case B01000000:    // Winch down at full speed
         analogWrite(WINCH_PWM, full_pwr);
+        digitalWrite(WINCH_BHI, HIGH);
         Serial.print("WINCH DOWN\n");
         break;
       
-      case B00011100:    // Winch down at half speed
+      case B00110000:    // Winch down at half speed
         analogWrite(WINCH_PWM, half_pwr);
+        digitalWrite(WINCH_BHI, HIGH);
         Serial.print("WINCH DOWN SLOW\n");
         break;
         
       default:          // Winch disabled
         analogWrite(WINCH_PWM, zero_pwr);
+        digitalWrite(WINCH_AHI, LOW);
+        digitalWrite(WINCH_BHI, LOW);
         Serial.print("SYSTEM IDLE\n");
         break;
     }
