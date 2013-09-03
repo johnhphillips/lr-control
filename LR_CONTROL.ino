2f8355bea8dbebdb1ea5f8fa9f628f92ac5c6941
John Phillips <john.h.phillips@clegadgetworks.com>
2013-09-03 10:59:47 -0700

    Added function to process input, analog read using timer, and fault detection.

--- LR_CONTROL.ino
+++ LR_CONTROL.ino
@@ -36,16 +36,53 @@
 #define FULL_PWR 225
 
 // State flags, all initially false
-volatile byte fault = LOW;
-volatile byte winch_active = LOW;
+volatile boolean fault = LOW;
+volatile boolean winch_active = LOW;
 
-volatile int current_input;
+volatile int current_input = 0;
 volatile int prev_input = 0;
 volatile long last_time = 0;
 
-void setup()  { 
+volatile int winch_current = 0;
+
+void timer1_init()
+{
+  cli();                   // global interrupt disable
+
+  // setup the 16-bit timer 1 
+  // TCCR1A - Timer1 / Counter Control Register A
+  TCCR1A = 0;              // Set register to 0
+  // TCCR1B - Timer1 / Counter Control Register B
+  TCCR1B = 0;              // Set register to 0
+  TCNT1 = 0;               // Initialize counter value to 0
+  // set compare match register for 10000Hz increments (0.1 msec)
+  OCR1A = 800 - 1;           
+  TCCR1B |= (1 << WGM12);  // Turn on CTC mode
+  // Set CS10 bit for no prescaler
+  TCCR1B |= (1 << CS10);
+  // enable timer1 compare interrupt
+  TIMSK1 |= (1 << OCIE1A);          
+
+  sei();                     // global interrupt enable
+  return; 
+}
+void adc_init()
+{
+  ADCSRA = 0;
+  ADCSRA |= ((1 << ADPS2)|(1 << ADPS0));//|(1 << ADPS0));                   // 8MHZ/2 = 4MHz the ADC reference clk
+  ADMUX |= (1 << REFS0);                    // Voltage reference from Avcc 3.3V
+  ADCSRA |= (1 << ADEN);                    // Turn on ADC
+  ADCSRA |= (1 << ADSC);                    // Perform initial converesion
+}
+
+void setup()  
+{ 
   // Setup serial output for debugging
   Serial.begin(9600);
+  // Setup ADC for current sensor sampling
+  adc_init();
+  // Setup Timer1 for 0.1ms sample rate
+  timer1_init();
   
   // Control box input, active low
   pinMode(CONTROL_A0, INPUT);
@@ -73,7 +110,8 @@ void setup()  {
   pinMode(FAULT_A2, OUTPUT);
 } 
 
-void read_input() {
+void read_input() 
+{
   // Read the state of the control inputs into a local variable:
   int reading = PIND & B01110000;
 
@@ -89,45 +127,68 @@ void read_input() {
     current_input = reading;
   }  
   prev_input = reading;
+  return;
+}
+
+void process_input()
+{
+  // check that not faulted, then process input 
+ if( !fault) {
+   switch ( current_input) {
+     case B01100000:    // Winch up at full speed
+       analogWrite(WINCH_PWM, FULL_PWR);
+       digitalWrite(WINCH_AHI, HIGH);    
+//       Serial.println("WINCH UP");
+       break;
+         
+     case B01010000:    // Winch up at half speed
+       analogWrite(WINCH_PWM, HALF_PWR);
+       digitalWrite(WINCH_AHI, HIGH);
+//       Serial.println("WINCH UP SLOW");
+       break; 
+        
+     case B01000000:    // Winch down at full speed
+       analogWrite(WINCH_PWM, FULL_PWR);
+       digitalWrite(WINCH_BHI, HIGH);
+//       Serial.println("WINCH DOWN");
+       break;
+        
+     case B00110000:    // Winch down at half speed
+       analogWrite(WINCH_PWM, HALF_PWR);
+       digitalWrite(WINCH_BHI, HIGH);
+//       Serial.println("WINCH DOWN SLOW");
+       break;
+          
+     default:          // Winch disabled
+       analogWrite(WINCH_PWM, ZERO_PWR);
+       digitalWrite(WINCH_AHI, LOW);
+       digitalWrite(WINCH_BHI, LOW);
+//       Serial.println("SYSTEM IDLE");
+       break;
+    }
+  }
+  return;
 }
 
 void loop() {
- // read control input 
- read_input(); 
- // check that not faulted, then process input 
- if( !fault) {
- switch ( current_input){
-      case B01100000:    // Winch up at full speed
-        analogWrite(WINCH_PWM, FULL_PWR);
-        digitalWrite(WINCH_AHI, HIGH);    
-//        Serial.println("WINCH UP");
-        break;
-       
-      case B01010000:    // Winch up at half speed
-        analogWrite(WINCH_PWM, HALF_PWR);
-        digitalWrite(WINCH_AHI, HIGH);
-//        Serial.println("WINCH UP SLOW");
-        break; 
-      
-      case B01000000:    // Winch down at full speed
-        analogWrite(WINCH_PWM, FULL_PWR);
-        digitalWrite(WINCH_BHI, HIGH);
-//        Serial.println("WINCH DOWN");
-        break;
-      
-      case B00110000:    // Winch down at half speed
-        analogWrite(WINCH_PWM, HALF_PWR);
-        digitalWrite(WINCH_BHI, HIGH);
-//       Serial.println("WINCH DOWN SLOW");
-        break;
-        
-      default:          // Winch disabled
-        analogWrite(WINCH_PWM, ZERO_PWR);
-        digitalWrite(WINCH_AHI, LOW);
-        digitalWrite(WINCH_BHI, LOW);
-//        Serial.println("SYSTEM IDLE");
-        break;
-    }
- }
+  // read control input 
+  read_input(); 
+  // process control input
+  process_input();
+ 
+  Serial.println(winch_current);
+  if( winch_current > OVERCURRENT && !fault) {
+    // diable timer1 compare interrupt
+    TIMSK1 &= ~(1 << OCIE1A);
+    digitalWrite(FAULT_A0, HIGH);
+    fault = HIGH;
+  } 
+}
+
+// Timer1 interrupt service routine
+ISR(TIMER1_COMPA_vect) 
+{
+  winch_current = analogRead(WINCH_CURRENT);            // read state of winch current
+  digitalWrite( FAULT_A0, !digitalRead(FAULT_A0));    // used for debugging
 }
 
